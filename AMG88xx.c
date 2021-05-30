cae50e78d082a54b7fb016a3dcb7a077a79867cb
imcort <179376503@qq.com>
2021-05-30 21:25:52 +0800

    it's ok.

---
+++ AMG88xx.c
@@ -0,0 +1,43 @@
+#include "AMG88xx.h"
+
+#include "transfer_handler.h"
+
+void amg88xx_begin(void) {
+	
+		uint8_t command[2];
+    //Set normal mode
+		command[0] = AMG88xx_PCTL;
+		command[1] = AMG88xx_MODE;
+    iic_send(AMG88xx_ADDR, command, 2, false);
+    //Power on reset
+		command[0] = AMG88xx_RST;
+		command[1] = AMG88xx_RESET;
+    iic_send(AMG88xx_ADDR, command, 2, false);
+    //Set 1hz refresh
+		command[0] = AMG88xx_FPSC;
+		command[1] = AMG88xx_REFRESH;
+    iic_send(AMG88xx_ADDR, command, 2, false);
+    
+    
+}
+
+void amg88xx_getIRGrid(int16_t* temp_grid) {
+		
+		uint8_t command[2];
+		uint8_t recv_buf[128];
+    //Set normal mode
+		command[0] = 0x80;
+    iic_send(AMG88xx_ADDR, command, 1, true);
+		iic_read(AMG88xx_ADDR, recv_buf, 128);
+	
+		for(int i=0; i < 64; i++)
+		{
+			
+				temp_grid[i] = (((int16_t)recv_buf[i*2+1]) << 4) | (((int16_t)recv_buf[i*2]) << 4);
+		
+		}
+
+
+}
+
+
---
+++ AMG88xx.h
@@ -0,0 +1,20 @@
+#ifndef AMGXX_H
+#define	AMGXX_H
+
+#include "stdint.h"
+
+#define AMG88xx_ADDR    0x69 //the I2C address by default
+//Some AMG Registers
+#define AMG88xx_PCTL    0x00
+#define AMG88xx_RST     0x01
+#define AMG88xx_FPSC    0x02
+#define AMG88xx_TTHL    0x0E //Thermistor
+//Some AMG commands
+#define AMG88xx_MODE    0x00 //Normal operating mode
+#define AMG88xx_REFRESH 0x00 //10Hz refresh rate
+#define AMG88xx_RESET   0x3F //Software reset
+
+void amg88xx_begin(void);
+void amg88xx_getIRGrid(int16_t* temp_grid);
+
+#endif
